99adaba0c757472bfbcc9d7f62d55382ac5b66b0
Orville <info@orville.be>
2020-04-14 17:23:09 +0200

    adds the style changes from Tim's comments on Python coding

--- Scripts/CollisionDetection/CollisionDetection.py
+++ Scripts/CollisionDetection/CollisionDetection.py
@@ -1,16 +1,22 @@
 #Author-An Pirlot
-#Description-Automatically run collision detection for multiple positions 
+#Description-Runs a number of simulations automatically 
 
-import adsk.core, adsk.fusion, adsk.cam, traceback, csv, os, sys
+from adsk import core
+from adsk.fusion import Component, Design, BRepBody, Occurrences, DesignTypes, InterferenceResult
+import traceback
+import csv
+import os
+from typing import List
+
 
 def run(context):
     try:
         # Fusion data
         global app, ui
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
+        app = core.Application.get()
+        ui = app.userInterface
         product = app.activeProduct
-        design = adsk.fusion.Design.cast(product)
+        design = Design.cast(product)
 
         if not design:
             ui.messageBox('This is not supported in the current workspace, please change to Design and try again.')
@@ -21,51 +27,51 @@ def run(context):
         if not document.isSaved:
             ui.messageBox('This is not supported for unsaved documents, please save and try again.')
             return
+
         if document.isModified:
             document.save('Auto save because of RunSimulations.py')
-        
+
         # Logging data
         home_folder = os.path.expanduser('~')
         filepath = os.path.join(home_folder, "Desktop", "CollisionDetection.csv")
-        
+
         rootComp = design.rootComponent
         results = checkAssemblyForInterference(rootComp)
-        logInterferenceData(filepath,results)
-
+        logInterferenceData(filepath, results)
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 
-def logInterferenceData(filepath, interferenceResults: list):
+def logInterferenceData(filepath, interferenceResults):  # type: (str, List[InterferenceResult]) -> None
     '''
     Writes the results of an interferencesimulation to a specified csv file
     '''
     writeToFile(filepath, ['TEST', 'STATUS', 'RESULT'])
 
-    if len(interferenceResults) == 0:
-        writeToFile(filepath,['Interference', 'OK', 'no interference'])
+    if not len(interferenceResults):
+        writeToFile(filepath, ['Interference', 'OK', 'no interference'])
     else:
         rowResult = 'interference between:'
         for result in interferenceResults:
-            entity1 = adsk.fusion.BRepBody.cast(result.entityOne)
-            entity2 = adsk.fusion.BRepBody.cast(result.entityTwo)
+            entity1 = BRepBody.cast(result.entityOne)
+            entity2 = BRepBody.cast(result.entityTwo)
             entities = '({component1}.{body1}, {component2}.{body2})'.format(component1=entity1.parentComponent.name, body1=entity1.name, component2=entity2.parentComponent.name, body2=entity2.name)
-            rowResult = ' '.join([rowResult,entities])
+            rowResult = ' '.join([rowResult, entities])
         writeToFile(filepath, ['Interference', 'NOK', rowResult])
 
 
-def checkAssemblyForInterference(rootComponent: adsk.fusion.Component):
+def checkAssemblyForInterference(rootComponent):  # type: (Component) -> List[InterferenceResult]
     """
     checks all bodies, incl bodies from subassemblies, for interference
     Return a list of interferenceResult objects, 
     if there is no interference detected, an empty list is returned
     """
     design = rootComponent.parentDesign
-    design.designType = adsk.fusion.DesignTypes.DirectDesignType
+    design.designType = DesignTypes.DirectDesignType
     bodies = getAllBodiesFromAssembly(rootComponent)
 
-    inputBodies = adsk.core.ObjectCollection.create()
+    inputBodies = core.ObjectCollection.create()
     for body in bodies:
         inputBodies.add(body)
 
@@ -73,15 +79,15 @@ def checkAssemblyForInterference(rootComponent: adsk.fusion.Component):
     if len(bodies) > 1:
         interferenceInput = design.createInterferenceInput(inputBodies)
         interferenceResults = design.analyzeInterference(interferenceInput)
-        for j in range (0, interferenceResults.count):
+        for j in range(interferenceResults.count):
             results.append(interferenceResults.item(j))
 
     # Undo 'Do not capture design history' to get the design history back
-    cmd = ui.commandDefinitions.itemById('UndoCommand').execute()
+    ui.commandDefinitions.itemById('UndoCommand').execute()
     return results
 
 
-def getAllBodiesFromAssembly(rootComponent: adsk.fusion.Component):
+def getAllBodiesFromAssembly(rootComponent):  # type: (Component) -> List[BRepBody]
     """
     Return a list of all the bodies in an assembly,
     including bodies from subassemblies,
@@ -89,23 +95,25 @@ def getAllBodiesFromAssembly(rootComponent: adsk.fusion.Component):
     """
     bodies = []
     bodies.extend(rootComponent.bRepBodies)
-    return __getAllBodiesFromOccurences(rootComponent.occurrences, bodies)
-
-
-def __getAllBodiesFromOccurences(occurences: adsk.fusion.Occurrences, bodies: list):
-    """
-    returns a list of all the bodies in an occurences object, 
-    including all bodies from child occurences
-    """
-    for j in range(0,occurences.count):
-        occurence = occurences.item(j)
-        bodies.extend(occurence.bRepBodies)
-        if occurence.childOccurrences:
-            __getAllBodiesFromOccurences(occurence.childOccurrences, bodies)
+    bodies += __getAllBodiesFromOccurences(rootComponent.occurrences)
     return bodies
 
 
-def writeToFile(filepath: str, row: list):
-    with open (filepath, 'a') as resultFile:
+def __getAllBodiesFromOccurences(occurences):  # type: (Occurrences) -> List[BRepBody]
+    """
+    returns a list of all the bodies in an occurences object,
+    including all bodies from child occurences
+    """
+    bodies = []
+    for j in range(occurences.count):
+        occurence = occurences.item(j)
+        bodies.extend(occurence.bRepBodies)
+        if occurence.childOccurrences:
+            bodies += __getAllBodiesFromOccurences(occurence.childOccurrences)
+    return bodies
+
+
+def writeToFile(filepath, row):  # type: (str, List[str]) -> None
+    with open(filepath, 'a') as resultFile:
         resultWriter = csv.writer(resultFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         resultWriter.writerow(row)
