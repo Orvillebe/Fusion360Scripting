bbb5a94187c02a8d3c50ff85ec793ace4ee30e98
Orville <info@orville.be>
2020-05-22 17:50:17 +0200

    adds updatetextsonsave which does the same as updateparametersintextsketches but automatically on save

---
+++ AddIns/UpdateTextSketchesOnSave/UpdateTextSketchesOnSave.py
@@ -0,0 +1,75 @@
+#Author-An Pirlot
+#Description-Update text in sketches according to a parameter with the same name. Example: the text in a sketch with name "Height" will be updated to "Height:Value" with the value from user parameter with name "Height".\t\t\t
+
+import adsk.core, adsk.fusion, adsk.cam, traceback, datetime
+
+ui = None
+handlers = []
+
+def updateTextSketchesInComponent(component):
+    for sketch in component.sketches:
+        # Check if there is a user parameter with the same name
+        design = component.parentDesign
+        userParameters = design.userParameters
+        parameter = userParameters.itemByName(sketch.name)
+        # If there is replace the text in the sketch by the name and value of the parameter
+        if parameter is not None:
+            value = parameter.value
+            for text in sketch.sketchTexts:
+                text.text = ("%s:%g%s" % (parameter.name, parameter.value, parameter.unit))
+
+
+class MyDocumentSavingHandler(adsk.core.DocumentEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        try:
+            # Uncomment following for ui message saying the script is busy updating the sketch texts
+            #ui.messageBox('Updating texts in sketches')
+
+            eventArgs = adsk.core.DocumentEventArgs.cast(args)
+            doc = adsk.fusion.FusionDocument.cast(eventArgs.document)
+
+            design = doc.design
+
+            if eventArgs.document.classType() != adsk.fusion.FusionDocument.classType(): 
+                ui.messageBox('Not a fusion document')
+                return
+
+            for component in design.allComponents:
+                updateTextSketchesInComponent(component)
+                
+        except Exception as e:
+            message = 'Failed:\n{}'.format(traceback.format_exc())
+
+            if ui:
+                ui.messageBox(message)
+
+
+
+def run(context):
+    try:
+        app = adsk.core.Application.get()
+        global ui
+        ui  = app.userInterface
+
+        onDocumentSaving = MyDocumentSavingHandler()
+        app.documentSaving.add(onDocumentSaving)
+        handlers.append(onDocumentSaving)
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+def stop(context):
+    try:
+        app = adsk.core.Application.get()
+        for handler in handlers:
+            app.documentSaving.remove(handler)
+
+        handler.clear()
+        ui = None
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
